cde1910beb07152c091651b4b1cdcfc926350816
JunSeok Lee <hansanlee1208@gmail.com>
2024-07-01 23:47:37 +0900

    BOJ 11052 Week01-1

--- CodingTest/week01/BOJ_11052.py
+++ CodingTest/week01/BOJ_11052.py
@@ -7,4 +7,6 @@ N_list = l_list[:]
 for i in range(N):
     for j in range(i):
         N_list[i] = max(N_list[i], N_list[i-j-1] + l_list[j])
-        print(i, N_list[i])
+        #점화식 확인
+        # print(i, N_list[i])
+print(N_list[N-1])
